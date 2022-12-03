638aaefa3b18283b732bc1c42394d2f5a50aa2b5
Christian Koop <contact@sprax2013.de>
2022-12-03 14:56:32 +0100

    Show loading status while espresso is running and add WebWorker checkbox
    + Remove unneded event listeners on worker, when
    receiving a message/error.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ document.getElementById('espresso_run')
       // regex src: https://stackoverflow.com/a/43766456
       const userArgs = [...argsString.matchAll(/("[^"\\]*(?:\\[\S\s][^"\\]*)*"|'[^'\\]*(?:\\[\S\s][^'\\]*)*'|\/[^\/\\]*(?:\\[\S\s][^\/\\]*)*\/[gimy]*(?=\s|$)|(?:\\\s|\S)+)/g)].map(m => m[0]);
 
-      const espressoResult = await executeEspresso(input, userArgs);
-
-      let exit_code = espressoResult.exitCode;
-      let stdout = espressoResult.stdout;
-
       const output = document.getElementById('output');
-      output.value = stdout;
+      try {
+        const espressoResult = await executeEspresso(input, userArgs);
+        output.value = espressoResult.stdout;
+      } catch (err) {
+        output.value = 'Error';
+        throw err;
+      }
     }, {passive: true});
 
 let workerRunning = false;
@@ -30,11 +31,17 @@ async function executeEspresso(input, args) {
     throw new Error('Worker is already busy');
   }
   workerRunning = true;
+  setLoadingUi(workerRunning);
+  await sleep(0);
 
-  if (!window.Worker) {
+  const shouldUseWebWorker = document.getElementById('use_web_worker').checked;
+  if (!shouldUseWebWorker || !window.Worker) {
     const {executeEspresso} = require('./worker');
     const espressoResult = await executeEspresso(input, args);
+
     workerRunning = false;
+    setLoadingUi(workerRunning);
+
     return espressoResult;
   }
 
@@ -45,14 +52,39 @@ async function executeEspresso(input, args) {
   return new Promise((resolve, reject) => {
     worker.postMessage({input, args});
 
-    worker.addEventListener('message', (event) => {
+    let messageListener;
+    const errorListener = (event) => {
       workerRunning = false;
+      setLoadingUi(workerRunning);
 
-      resolve(event.data);
-    }, {once: true, passive: true});
-    worker.addEventListener('error', (event) => {
-      workerRunning = false;
+      worker.removeEventListener('message', messageListener, {once: true, passive: true});
       reject(event);
-    }, {once: true, passive: true});
+    };
+    messageListener = (event) => {
+      workerRunning = false;
+      setLoadingUi(workerRunning);
+
+      worker.removeEventListener('error', errorListener, {once: true, passive: true});
+      resolve(event.data);
+    };
+
+
+    worker.addEventListener('message', messageListener, {once: true, passive: true});
+    worker.addEventListener('error', errorListener, {once: true, passive: true});
   });
+}
+
+function setLoadingUi(loading) {
+  if (loading) {
+    document.getElementById('espresso_run').setAttribute('disabled', '');
+    document.getElementById('output').setAttribute('disabled', '');
+    document.getElementById('output').value = 'Loading...';
+  } else {
+    document.getElementById('espresso_run').removeAttribute('disabled');
+    document.getElementById('output').removeAttribute('disabled');
+  }
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
