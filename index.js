a5a82ba1e19393a5d2dceb0d667fb43976c98ddd
Christian Koop <contact@sprax2013.de>
2022-12-03 12:44:12 +0100

    Implements first prototype for running espresso in web workers

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 'use strict';
 
-const {init, WASI} = require('@wasmer/wasi');
+document.getElementById('espresso_run')
+    .addEventListener('click', async () => {
+      const input = document.getElementById('input').value;
+      const argsString = document.getElementById('args').value;
 
-const module_cache = {};
+      // regex src: https://stackoverflow.com/a/43766456
+      const userArgs = [...argsString.matchAll(/("[^"\\]*(?:\\[\S\s][^"\\]*)*"|'[^'\\]*(?:\\[\S\s][^'\\]*)*'|\/[^\/\\]*(?:\\[\S\s][^\/\\]*)*\/[gimy]*(?=\s|$)|(?:\\\s|\S)+)/g)].map(m => m[0]);
 
-async function get_module(modulename) {
-    if (module_cache[modulename]) {
-        return module_cache[modulename];
-    }
-    const wasm = await WebAssembly.compileStreaming(fetch(modulename));
-    module_cache[modulename] = wasm;
-    return wasm;
+      const espressoResult = await executeEspresso(input, userArgs);
+
+      let exit_code = espressoResult.exitCode;
+      let stdout = espressoResult.stdout;
+
+      const output = document.getElementById('output');
+      output.value = stdout;
+    }, {passive: true});
+
+let workerRunning = false;
+let worker;
+
+/**
+ * @param {string} input
+ * @param {string[]} args
+ * @return {Promise<{exitCode: number, stdout: string, stderr: string}>}
+ */
+async function executeEspresso(input, args) {
+  if (workerRunning) {
+    throw new Error('Worker is already busy');
+  }
+  workerRunning = true;
+
+  if (!window.Worker) {
+    const {executeEspresso} = require('./worker');
+    const espressoResult = await executeEspresso(input, args);
+    workerRunning = false;
+    return espressoResult;
+  }
+
+  if (!worker) {
+    worker = new Worker('./bundle-worker.js');
+  }
+
+  return new Promise((resolve) => {
+    const workerTask = {input, args};
+    console.log('Sending task to worker:', workerTask);
+    worker.postMessage(workerTask);
+
+    worker.addEventListener('message', (event) => {
+      workerRunning = false;
+
+      console.log('Received result from worker:', event.data);
+      resolve(event.data);
+    }, {once: true, passive: true});
+  });
 }
-
-async function get_instance(wasi, modulename) {
-    const module = await get_module(modulename);
-    const instance = await WebAssembly.instantiate(module, wasi.getImports(module));
-
-    await wasi.instantiate(instance, {});
-
-    return wasi;
-}
-
-async function main() {
-
-    await init();
-
-    document.getElementById("espresso_run").addEventListener("click", async () => {
-        const input = document.getElementById("input").value;
-        const argsString = document.getElementById("args").value;
-
-        // regex src: https://stackoverflow.com/a/43766456
-        const userArgs = [...argsString.matchAll(/("[^"\\]*(?:\\[\S\s][^"\\]*)*"|'[^'\\]*(?:\\[\S\s][^'\\]*)*'|\/[^\/\\]*(?:\\[\S\s][^\/\\]*)*\/[gimy]*(?=\s|$)|(?:\\\s|\S)+)/g)].map(m => m[0]);
-
-        let wasi = new WASI({env: {}, args: ["espresso", ...userArgs, "/input.esp"]});
-
-        let file = wasi.fs.open("/input.esp", {read: true, write: true, create: true});
-        file.writeString(input);
-        file.flush();
-
-        await get_instance(wasi, "espresso.wasm");
-
-        let exit_code = wasi.start();
-        let stdout = wasi.getStdoutString();
-
-        const output = document.getElementById("output");
-        output.value = stdout;
-    }, {
-        passive: true
-    });
-
-}
-
-main()
-    .catch((err) => {
-        console.error(err);
-        alert('Failed to load Espresso-WASM module');
-    });
---
+++ worker.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const {init, WASI} = require('@wasmer/wasi');
+
+const module_cache = {};
+let wsmInitialized = false;
+
+addEventListener('message', (event) => {
+  executeEspresso(event.data.input, event.data.args)
+      .then(postMessage);
+});
+
+/**
+ * @param {string} input
+ * @param {string[]} args
+ * @return {Promise<{exitCode: number, stdout: string, stderr: string}>}
+ */
+async function executeEspresso(input, args) {
+  if(!wsmInitialized) {
+    await init();
+    wsmInitialized = true;
+  }
+
+  const wasi = new WASI({env: {}, args: ['espresso', ...args, '/input.esp']});
+
+  const file = wasi.fs.open('/input.esp', {read: true, write: true, create: true});
+  file.writeString(input);
+  file.flush();
+
+  await instantiateModule(wasi, 'espresso.wasm');
+
+  const exitCode = wasi.start();
+  return {
+    exitCode,
+    stdout: wasi.getStdoutString(),
+    stderr: wasi.getStderrString()
+  };
+}
+
+/**
+ * @param {string} moduleName
+ * @return {Promise<WebAssembly.Module>}
+ */
+async function getModule(moduleName) {
+  if (module_cache[moduleName]) {
+    return module_cache[moduleName];
+  }
+
+  const wasm = await WebAssembly.compileStreaming(fetch(moduleName));
+  module_cache[moduleName] = wasm;
+  return wasm;
+}
+
+/**
+ * @param {WASI} wasi
+ * @param {string} moduleName
+ * @return {Promise<void>}
+ */
+async function instantiateModule(wasi, moduleName) {
+  const module = await getModule(moduleName);
+  const instance = await WebAssembly.instantiate(module, wasi.getImports(module));
+
+  await wasi.instantiate(instance, {});
+}
+
+exports.executeEspresso = executeEspresso;
